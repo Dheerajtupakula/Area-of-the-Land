f59689ab189642f06db9cdc7deacfa7fdcfef77a
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-06-26 10:55:13 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -226,7 +226,7 @@ export default function Home() {
       )}
       {data.length > 2 && (
         <button
-          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none"
+          className="inline-flex items-center justify-center mt-3 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none"
           onClick={handleMap}
         >
           View Map
