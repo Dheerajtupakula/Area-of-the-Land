ad9bea21570f95f7dc96ed7f89b560cae0304b04
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-06-26 10:51:28 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -153,75 +153,77 @@ export default function Home() {
   };
 
   return (
-    <main className="relative flex flex-col justify-center items-center gap-3 p-2">
-      <div>
-        {isCameraOpen && (
-          <div
-            className={`absolute inset-0 ${
-              isMobile ? "h-screen" : "h-full"
-            } flex justify-center items-center bg-black bg-opacity-50 z-50`}
-          >
-            <div className="flex  flex-col justify-center items-center bg-white p-4 rounded shadow-lg">
-              {devices.length > 0 && (
-                <Webcam
-                  audio={false}
-                  height={isMobile ? 480 : 360}
-                  width={isMobile ? 320 : 480}
-                  ref={webcamRef}
-                  screenshotFormat="image/jpeg"
-                  videoConstraints={{ deviceId: selectedCamera || undefined }}
-                />
-              )}
-              <button
-                className="mt-4 p-2 bg-blue-500 text-white rounded"
-                onClick={capture}
-                disabled={isLoading}
-              >
-                {isLoading ? "Capturing..." : `Capture ${currentDirection}`}
-              </button>
-
-              <button
-                className="mt-2 p-2 bg-gray-300 text-gray-800 rounded"
-                onClick={switchCamera}
-              >
-                Switch Camera
-              </button>
-
-              {isLoading && (
-                <div className="absolute text-white inset-0 z-50 bg-slate-700/75 flex justify-center items-center">
-                  {" "}
-                  Capturing...
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-        <div className="grid grid-cols-2 gap-4">
-          {directions.map((direction) => (
+    <main className="relative flex flex-col justify-center items-center gap-3 ">
+      {!isMapView && (
+        <div className="p-2">
+          {isCameraOpen && (
             <div
-              key={direction}
-              className="border p-4 flex justify-center items-center"
+              className={`absolute inset-0 ${
+                isMobile ? "h-screen" : "h-full"
+              } flex justify-center items-center bg-black bg-opacity-50 z-50`}
             >
-              {images[direction] ? (
-                <div className="size-20">
-                  <img
-                    className="w-full h-full object-cover"
-                    src={images[direction].image}
-                    alt={direction}
+              <div className="flex  flex-col justify-center items-center bg-white p-4 rounded shadow-lg">
+                {devices.length > 0 && (
+                  <Webcam
+                    audio={false}
+                    height={isMobile ? 480 : 360}
+                    width={isMobile ? 320 : 480}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={{ deviceId: selectedCamera || undefined }}
                   />
-                </div>
-              ) : (
+                )}
                 <button
-                  className="p-4 bg-blue-500 text-white rounded"
-                  onClick={() => handleDirectionClick(direction)}
+                  className="mt-4 p-2 bg-blue-500 text-white rounded"
+                  onClick={capture}
+                  disabled={isLoading}
                 >
-                  {direction.charAt(0).toUpperCase() + direction.slice(1)}
+                  {isLoading ? "Capturing..." : `Capture ${currentDirection}`}
                 </button>
-              )}
+
+                <button
+                  className="mt-2 p-2 bg-gray-300 text-gray-800 rounded"
+                  onClick={switchCamera}
+                >
+                  Switch Camera
+                </button>
+
+                {isLoading && (
+                  <div className="absolute text-white inset-0 z-50 bg-slate-700/75 flex justify-center items-center">
+                    {" "}
+                    Capturing...
+                  </div>
+                )}
+              </div>
             </div>
-          ))}
+          )}
+          <div className="grid grid-cols-2 gap-4">
+            {directions.map((direction) => (
+              <div
+                key={direction}
+                className="border p-4 flex justify-center items-center"
+              >
+                {images[direction] ? (
+                  <div className="size-20">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={images[direction].image}
+                      alt={direction}
+                    />
+                  </div>
+                ) : (
+                  <button
+                    className="p-4 bg-blue-500 text-white rounded"
+                    onClick={() => handleDirectionClick(direction)}
+                  >
+                    {direction.charAt(0).toUpperCase() + direction.slice(1)}
+                  </button>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
       {data.length > 2 && (
         <button
           className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none"
